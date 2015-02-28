e7cf3064af0eb37e94a202d509fa7b020b3484c6
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-02-28 17:42:22 +0100

    view: remove duplicated Gio import

--- gnomemusic/view.py
+++ gnomemusic/view.py
@@ -34,7 +34,6 @@
 from gi.repository import Gtk
 from gi.repository import GObject
 from gi.repository import Gd
-from gi.repository import Gio
 from gi.repository import Grl
 from gi.repository import Gio
 from gi.repository import Pango
